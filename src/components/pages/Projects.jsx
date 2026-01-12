30b38d018f9c163a98b286ada329df29f9380050
vaiozaffana <vaiozaffana@gmail.com>
2026-01-12 12:00:49 +0700

    chore: update and projects

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -7,7 +7,7 @@ import { GithubIcon } from "lucide-react";
 import Head from "next/head";
 import "@/styles/module.css";
 
-const FeaturedProject = ({ type, title, summary, img, link, github }) => {
+const FeaturedProject = ({ type, title, summary, img, github }) => {
   return (
     <article
       className="w-full flex items-center justify-between rounded-br-2xl rounded-3xl
@@ -19,27 +19,29 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
 rounded-br-3xl xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]"
       />
 
-      <Link
+      {/* <Link
         href={link}
         target="_blank"
         className="w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full"
-      >
-        <Image src={img} alt={title} className="w-full h-auto" />
-      </Link>
+      > */}
+        <div className="w-1/2 lg:w-full">
+          <Image src={img} alt={title} className="w-full h-auto rounded-lg object-cover" />
+        </div>
+      {/* </Link> */}
 
       <div className="w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6">
         <span className="text-primaryDark font-medium text-xl xs:text-base">
           {type}
         </span>
-        <Link
+        {/* <Link
           href={link}
           target="_blank"
           className="hover:underline underline-offset-2"
-        >
-          <h2 className="my-2 w-full text-left text-4xl font-bold sm:text-sm">
+        > */}
+          <h2 className="my-2 w-full text-left text-4xl font-bold lg:text-2xl md:text-xl sm:text-lg">
             {title}
           </h2>
-        </Link>
+        {/* </Link> */}
         <p className="my-2 font-medium text-slate-50 sm:text-sm">{summary}</p>
         <div className="mt-2 flex items-center">
           <Link
@@ -49,21 +51,21 @@ rounded-br-3xl xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]"
           >
             <GithubIcon />
           </Link>
-          <Link
+          {/* <Link
             href={link}
             target="_blank"
             className="ml-4 rounded-lg bg-white text-darkGray 
           p-2 px-6 text-lg font-semibold sm:px-4 sm:text-base"
           >
             Visit Project
-          </Link>
+          </Link> */}
         </div>
       </div>
     </article>
   );
 };
 
-const Project = ({ title, type, shortSummary, img, link, github }) => {
+const Project = ({ title, type, shortSummary, img, github }) => {
   return (
     <article
       className="w-full flex flex-col items-center justify-between rounded-2xl
@@ -74,30 +76,30 @@ const Project = ({ title, type, shortSummary, img, link, github }) => {
 rounded-br-3xl md:-right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]"
       />
 
-        <Image src={img} alt={title} className="w-full h-auto" />
+        <Image src={img} alt={title} className="w-full h-auto rounded-lg object-cover" />
 
       <div className="w-full flex flex-col items-start justify-between mt-4">
-        <span className="text-primaryDark font-medium text-xl lg:text-lg md:text-base">
+        <span className="text-primaryDark font-medium text-xl lg:text-lg md:text-base xs:text-sm">
           {type}
         </span>
-        <h2 className="my-2 w-full text-left text-3xl font-bold lg:text-2xl">
+        <h2 className="my-2 w-full text-left text-3xl font-bold lg:text-2xl md:text-xl sm:text-lg xs:text-base">
           {title}
         </h2>
-        <p className="my-2 font-medium text-slate-50 sm:text-sm">{shortSummary}</p>
+        <p className="my-2 font-medium text-slate-50 md:text-sm xs:text-xs">{shortSummary}</p>
         <div className="w-full mt-2 flex items-center justify-between">
-          <Link
+          {/* <Link
             href={link}
             target="_blank"
             className="text-lg font-semibold hover:underline md:text-base"
           >
             Visit
-          </Link>
+          </Link> */}
           <Link
             href={github}
             target="_blank"
-            className="w-10 h-10 flex bg-darkGray rounded-full font-bold items-center justify-center text-center cursor-pointer border-2 border-slate-100"
+            className="w-10 h-10 flex bg-darkGray rounded-full font-bold items-center justify-center text-center cursor-pointer border-2 border-slate-100 xs:w-8 xs:h-8"
           >
-            <GithubIcon />
+            <GithubIcon className="xs:w-4 xs:h-4" />
           </Link>
         </div>
       </div>
@@ -119,35 +121,41 @@ const Projects = () => {
             text="Imagination Trumps Knowledge"
           />
 
-          <div className="grid grid-cols-12 gap-24 gap-y-32 text-slate-50 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
+          <div className="grid grid-cols-12 gap-24 gap-y-32 text-slate-50 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0 sm:gap-y-16 xs:gap-y-12">
             <div className="col-span-12">
               <FeaturedProject
                 title="Volunify"
                 img="/Volunify - Landing Page.png"
                 summary="Volunify adalah platform yang menghubungkan relawan dengan berbagai kesempatan volunteer. Anda bisa bergabung dengan acara yang sesuai minat, menciptakan program volunteer sendiri, dan selalu up-to-date dengan daftar acara terbaru. Jadikan langkah pertama Anda untuk memberikan dampak positif lebih mudah dan menyenangkan dengan Volunify!"
-                link="https://volunify.vercel.app/"
                 github="https://github.com/dhiodhaha/volunify.git"
                 type="Featured Project"
               />
             </div>
-            <div className="col-span-6">
+            <div className="col-span-6 sm:col-span-12">
               <Project
-                title="BikinCerdas"
-                img="/Landing-Page-BikinCerdas-ukl.png"
-                link="https://github.com/vaiozaffana/Project_ukl.git"
-                shortSummary="Website mengenai kursus pengetahuan umum"
+                title="MoodyfAI"
+                img="/FeaturedProject1.jpeg"
+                shortSummary="Website AI rekomendasi musik atau lagu berdasarkan mood yang telah dimasukkan oleh pengguna."
                 github="https://github.com/vaiozaffana/Project_ukl.git"
                 type="Project"
               />
             </div>
-            <div className="col-span-6">
+            <div className="col-span-6 sm:col-span-12">
               <Project
-                title="Floruna"
-                img="/dasboard_floruna.png"
-                link="https://github.com/vaiozaffana/local-list-app.git"
-                shortSummary="Aplikasi mobile lokal mengenai flora dan fauna"
-                github="https://github.com/vaiozaffana/local-list-app.git"
+                title="SidoTanggap"
+                img="/Project1.png"
+                shortSummary="Platform digital yang memungkinkan masyarakat untuk melaporkan permasalahan terkait lingkungan dan sosial di sekitar mereka secara cepat dan efisien."
+                github="https://github.com/vaiozaffana/sido-tanggap.git"
                 type="Project"
+              />
+            </div>
+            <div className="col-span-12">
+              <FeaturedProject
+                title="WF Law Office"
+                img="/FeaturedProject2.png"
+                summary="Volunify adalah platform yang menghubungkan relawan dengan berbagai kesempatan volunteer. Anda bisa bergabung dengan acara yang sesuai minat, menciptakan program volunteer sendiri, dan selalu up-to-date dengan daftar acara terbaru. Jadikan langkah pertama Anda untuk memberikan dampak positif lebih mudah dan menyenangkan dengan Volunify!"
+                github="https://github.com/dhiodhaha/volunify.git"
+                type="Featured Project"
               />
             </div>
           </div>
